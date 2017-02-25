95bf1aa13492658059694589dfaed3b51135daf3
Daniel Whitston <dan@whitston.org.uk>
2017-02-25 12:28:05 +0000

    working remote webdriver, first test passing

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+ruby '2.3.1'
+
+group :test, :development do
+  gem 'sinatra'
+  gem 'rspec'
+  gem 'rspec-sinatra'
+  gem 'capybara'
+  gem 'selenium-webdriver'
+end
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,10 @@
+feature 'Enter names' do
+  scenario 'submitting names', :js => true  do 
+    visit('/')
+    save_and_open_page
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave vs. Mittens'
+  end
+end
